00119c73afe717dfaff9088845a2bd657db773c3
Alec Chan <alecchangod@gmail.com>
2024-07-15 17:33:31 +0800

    picasso: fix lunch menu for QPR2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_picasso.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_picasso-user \
-    lineage_picasso-userdebug \
-    lineage_picasso-eng
+    lineage_picasso-ap1a-user \
+    lineage_picasso-ap1a-userdebug \
+    lineage_picasso-ap1a-eng
